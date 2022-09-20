0fd1600df12b59c73dde437d0fcb5fb58d777e0f
Tim Repke <repke@mcc-berlin.net>
2022-09-20 17:40:04 +0200

    add dotenv to requirements to enable connecting with settings from file

--- src/nacsos_data/db/__init__.py
+++ src/nacsos_data/db/__init__.py
@@ -1,4 +1,4 @@
 from .engine import DatabaseEngine, DatabaseEngineAsync
+from .connection import get_engine, get_engine_async
 
-
-__all__ = ['DatabaseEngine', 'DatabaseEngineAsync']
+__all__ = ['DatabaseEngine', 'DatabaseEngineAsync', 'get_engine_async', 'get_engine']
