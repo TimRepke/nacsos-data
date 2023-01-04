11219869c62c5554d93162898fd6535c7c382101
Tim Repke <repke@mcc-berlin.net>
2023-01-04 17:20:34 +0100

    fix database connection url

--- src/nacsos_data/db/engine.py
+++ src/nacsos_data/db/engine.py
@@ -1,14 +1,19 @@
 import json
+import logging
 from typing import AsyncIterator, Iterator, Any
 from json import JSONEncoder
+
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text, URL
 from contextlib import contextmanager, asynccontextmanager
 from datetime import datetime
 
 # unused import required so the engine sees the models!
 from . import schemas  # noqa F401
+
+logger = logging.getLogger('nacsos_data.engine')
 
 
 class DictLikeEncoder(JSONEncoder):
@@ -34,19 +39,31 @@ class DatabaseEngineAsync:
         self._database = database
 
         # TODO expire_on_commit (check if this should be turned off)
-        self._connection_str = f'postgresql+psycopg://{self._user}:{self._password}@' \
-                               f'{self._host}:{self._port}/{self._database}'
+        self._connection_str = URL.create(
+            drivername='postgresql+psycopg',
+            username=self._user,
+            password=self._password,
+            host=self._host,
+            database=self._database,
+        )
         self.engine = create_async_engine(self._connection_str, echo=True, future=True,
                                           json_serializer=DictLikeEncoder().encode)
         self._session: async_sessionmaker[AsyncSession] = async_sessionmaker(  # type: ignore[type-arg] # FIXME
             bind=self.engine, autoflush=False, autocommit=False)
 
-    def startup(self) -> None:
+    async def startup(self) -> None:
         """
         Call this function to initialise the database engine.
         """
-        # SQLModel.metadata.create_all(self.engine)
-        pass
+        try:
+            logger.debug('AsyncEngine starting up...')
+            logger.info(f'AsyncEngine connecting to {self._user}:****@{self._host}:{self._port}/{self._database}')
+            session: AsyncSession = self._session()
+            await session.execute(text('SELECT 1;'))
+            logger.error('Connection seems to be ready.')
+        except OperationalError as e:
+            logger.error('Connection failed!')
+            logger.exception(e)
 
     @asynccontextmanager
     async def session(self) -> AsyncIterator[AsyncSession]:
@@ -76,8 +93,13 @@ class DatabaseEngine:
         self._database = database
 
         # TODO expire_on_commit (check if this should be turned off)
-        self._connection_str = f'postgresql+psycopg2://{self._user}:{self._password}@' \
-                               f'{self._host}:{self._port}/{self._database}'
+        self._connection_str = URL.create(
+            drivername='postgresql+psycopg',
+            username=self._user,
+            password=self._password,
+            host=self._host,
+            database=self._database,
+        )
         self.engine = create_engine(self._connection_str, echo=True, future=True,
                                     json_serializer=DictLikeEncoder().encode)
         self._session: sessionmaker[Session] = sessionmaker(  # type: ignore[type-arg] # FIXME
