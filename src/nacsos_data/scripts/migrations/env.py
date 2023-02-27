cfd17231c0e776d6039794daf2b616826d5a1c98
Tim Repke <repke@mcc-berlin.net>
2023-02-27 20:11:12 +0100

    fix path

--- src/nacsos_data/scripts/migrations/env.py
+++ src/nacsos_data/scripts/migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool, URL
 
 from alembic import context
 
-from src.nacsos_data.db.schemas import Base
+from nacsos_data.db.schemas import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
